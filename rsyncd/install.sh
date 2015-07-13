8eab68843cb93e81a004c67db3c1d959992bc820
bee002 <bee002@tom.com>
2015-07-13 16:34:56 +0800

    rsyncd version 1.10

--- rsyncd/install.sh
+++
@@ -1,8 +0,0 @@
-mkdir /var/log/rsync 2>/dev/null
-mkdir /etc/rsync 2>/dev/null
-chmod +x rsyncd
-chmod 600 rsyncd.secrets
-cp rsyncd /etc/init.d/
-cp rsyncd.conf /etc/rsync/
-cp rsyncd.secrets /etc/rsync/
-
